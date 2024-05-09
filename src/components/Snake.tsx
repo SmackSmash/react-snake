3ef15f473aa97e6f94248a78da929fb280214703
Dan Moffat <danmoffat@gmail.com>
2024-05-09 16:25:17 +0100

    Basic rendering of snake to grid

--- src/components/Snake.tsx
+++ src/components/Snake.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 
 const snakeConfig = {
   dimensions: {
-    x: 40,
-    y: 30
+    x: 30,
+    y: 25
   }
 };
 
@@ -11,16 +11,36 @@ const Snake = () => {
   const [grid, setGrid] = useState(
     new Array(snakeConfig.dimensions.y).fill(new Array(snakeConfig.dimensions.x).fill(0))
   );
+  const [snake, setSnake] = useState([
+    [2, 2],
+    [2, 3],
+    [2, 4]
+  ]);
 
   return (
     <div className='container'>
       <h1>Snake</h1>
       <div className='w-full'>
-        {grid.map(arry => (
-          <div className='flex'>
-            {arry.map(arrx => (
-              <div className='m-[1px] h-[10px] w-[10px] bg-poimandres-darkslate'></div>
-            ))}
+        {grid.map((arry: number[], y: number) => (
+          <div className='flex' key={`row${y}`}>
+            {arry.map((_arrx: number, x: number) => {
+              for (const coord of snake) {
+                if (coord[0] === x && coord[1] === y) {
+                  return (
+                    <div
+                      className='m-[1px] h-[10px] w-[10px] bg-poimandres-lightgreen'
+                      key={`${x}${y}`}
+                    ></div>
+                  );
+                }
+              }
+              return (
+                <div
+                  className='m-[1px] h-[10px] w-[10px] bg-poimandres-blackslate'
+                  key={`${x}${y}`}
+                ></div>
+              );
+            })}
           </div>
         ))}
       </div>
