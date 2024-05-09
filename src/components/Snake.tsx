63a07da01768fb22a6e4311976d72c113fbc411b
Dan Moffat <danmoffat@gmail.com>
2024-05-09 17:56:27 +0100

    Basic snake movement implemented

--- src/components/Snake.tsx
+++ src/components/Snake.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
+
+type Action = { type: 'down' };
 
 const snakeConfig = {
   dimensions: {
     x: 30,
-    y: 25
+    y: 30
+  }
+};
+
+const snakeReducer = (state: any, action: Action) => {
+  switch (action.type) {
+    case 'down':
+      return state.map((coord: number[]) => [coord[0], coord[1] + 1]);
+    default:
+      return state;
   }
 };
 
@@ -11,15 +22,27 @@ const Snake = () => {
   const [grid, setGrid] = useState(
     new Array(snakeConfig.dimensions.y).fill(new Array(snakeConfig.dimensions.x).fill(0))
   );
-  const [snake, setSnake] = useState([
+  const [direction, setDirection] = useState('down');
+  const [snake, dispatch] = useReducer(snakeReducer, [
     [2, 2],
     [2, 3],
     [2, 4]
   ]);
 
+  const handleStart = () => {
+    setInterval(() => {
+      switch (direction) {
+        case 'down':
+        default:
+          dispatch({ type: 'down' });
+      }
+    }, 250);
+  };
+
   return (
     <div className='container'>
       <h1>Snake</h1>
+      <button onClick={handleStart}>Start</button>
       <div className='w-full'>
         {grid.map((arry: number[], y: number) => (
           <div className='flex' key={`row${y}`}>
