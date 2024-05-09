63a430d1a3b95327d01bc12d73f55e2faa373117
Dan Moffat <danmoffat@gmail.com>
2024-05-09 14:56:52 +0100

    Basic grid setup

--- src/components/Snake.tsx
+++ src/components/Snake.tsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+
+const snakeConfig = {
+  dimensions: {
+    x: 40,
+    y: 30
+  }
+};
+
 const Snake = () => {
-  return <h1>Snake</h1>;
+  const [grid, setGrid] = useState(
+    new Array(snakeConfig.dimensions.y).fill(new Array(snakeConfig.dimensions.x).fill(0))
+  );
+
+  return (
+    <div className='container'>
+      <h1>Snake</h1>
+      <div className='w-full'>
+        {grid.map(arry => (
+          <div className='flex'>
+            {arry.map(arrx => (
+              <div className='m-[1px] h-[10px] w-[10px] bg-poimandres-darkslate'></div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Snake;
